2a52413442e64d8b4e1653e6274003b9ae646d50
Pavel <pl.vasichkin@gmail.com>
2018-07-03 16:45:47 +0300

    Add<ThreadException>

--- ThreadException/src/Program.java
+++ ThreadException/src/Program.java
@@ -1,22 +1,51 @@
-
+//---------------Почему-то не работает 
 public class Program {
 
 	public static void main(String[] args) throws InterruptedException {
 		
 		Thread t = new Thread() {
+			
 			public void run() {
 				System.out.println("Sleeping...");
 				try {
 					Thread.sleep(1000);
 				} catch (InterruptedException e) {
-					System.out.println("Interrupt");
+					System.out.println("Interrupted.");
 				}
-				System.out.printf("Interrupted exception from thread %s\n",this.getName());
+				System.out.printf("Throwing exception from thread %s\n",
+						this.getName());
 				throw new RuntimeException();
 			}
 		};
 		
+		// Обработчик ошибок для одного потока
+		/*Thread.UncaughtExceptionHandler h = new Thread.UncaughtExceptionHandler() {	
+			@Override
+			public void uncaughtException(Thread th, Throwable e) {
+				System.out.printf("Uncaught exception from thread %s handled by thread %s : (specific) %s\n",
+						th.getName(), Thread.currentThread().getName(), e);
+				
+			}
+		};
+		t.setUncaughtExceptionHandler(h);
 		
+		//Универсальный обработчик ошибок для нескольких потоков
+		Thread.setDefaultUncaughtExceptionHandler( // статический метод
+				new Thread.UncaughtExceptionHandler() {
+					
+					@Override
+					public void uncaughtException(Thread th, Throwable ex) {
+						
+						System.out.printf("Uncaught exception from thread %s handled by thread %s : (unspecific) %s\n",
+								th.getName(), Thread.currentThread().getName(), ex);					
+					}
+		});*/
+		
+		t.start();
+		
+		Thread.sleep(5000);
+		
+		System.out.println("main");
 		
 		
 	}
